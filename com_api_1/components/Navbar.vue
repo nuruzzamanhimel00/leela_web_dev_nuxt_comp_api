863cfca44a58bd5e755f7147d1e7d884271dfb68
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2023-11-18 19:04:45 +0600

    init

---
+++ com_api_1/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul class="nav">
+    <li class="nav-item">
+      <a class="nav-link active" href="#">Active</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link disabled">Disabled</a>
+    </li>
+  </ul>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped></style>
